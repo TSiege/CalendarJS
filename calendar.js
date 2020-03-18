876231abc6f43c4b215f3058fc975a9b19fc9c63
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-03-18 14:25:27 -0400

    get month rendering

---
+++ calendar.js
@@ -0,0 +1,55 @@
+function getFirstOfMonth(month = new Date()) {
+  month.setDate(1)
+  return month
+}
+
+function getDaysInMonth(month = MONTH) {
+  var date = new Date(month.getTime())
+  var days = []
+  while (date.getMonth() === month.getMonth()) {
+    days.push(new Date(date))
+    date.setDate(date.getDate() + 1)
+  }
+  return days
+}
+
+function renderDateTimeStr(date) {
+  return new Intl.DateTimeFormat(navigator.language).format(date)
+}
+
+function generateDaysHtml(month) {
+  const days = getDaysInMonth(month)
+  return days.map(day => {
+      return `
+        <button class="${}">
+          <time datetime="${renderDateTimeStr(day)}">${day.getDate()}</time>
+        </button>
+      `
+    })
+    .join('\n')
+}
+
+function renderDays(month) {
+  const daysHtml = generateDaysHtml(month)
+  const dateGrid = document.querySelector('.date-grid')
+  dateGrid.innerHTML = daysHtml
+  const firstDay = document.querySelector('.date-grid button:first-child')
+  firstDay.style['grid-column'] = (month.getDay() + 1)
+}
+
+
+function renderCalendar() {
+  const month = getFirstOfMonth()
+  renderDays(month)
+
+}
+
+function onReady(fn) {
+  if (document.readyState !== 'loading') {
+    fn()
+  } else {
+    document.addEventListener('DOMContentLoaded', fn)
+  }
+}
+
+onReady(renderCalendar)
